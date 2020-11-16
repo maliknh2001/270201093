3450812d74d6dcf63410942835748aa0b67c7408
r48982982 <malikal@ogr365.iyte.edu.tr>
2020-11-16 09:15:16 +0000

    I did second exercize

--- lab1/class4.py
+++ lab1/class4.py
@@ -1,3 +1,13 @@
+#first exersize
 a=int(input("insert an integer"))
 for i in range(1,11):
   print(str(a), "X", i, "=" , a*i)
+#exersize 2
+b = int(input("how many numbers? "))
+even_num=0
+for i in range(b):
+  c=int(input("Enter an integer: "))
+  if c%2 == 0 :
+    even_num+=1
+result= (even_num /b) * 100
+print(str(result)+"%")
