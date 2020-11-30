9a0f28af1d04d51fb7b8d6764b7bab7b448a0027
r48982982 <malikal@ogr365.iyte.edu.tr>
2020-11-30 08:40:22 +0000

    exersize 1 answer

---
+++ Lab7/answers.py
@@ -0,0 +1,8 @@
+#checking code is working:
+print("hello world")
+#exercize 1
+people= [("Jon",15), ("Ned",45), ("Arya",9), ("Catelyn",44),("Bran",10)]
+for i in people:
+  if i[1] > 18:
+    print(i[0])
+  
