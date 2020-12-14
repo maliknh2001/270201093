2f9990c4c0efcdd9597638ebb062f6530c414983
r48982982 <malikal@ogr365.iyte.edu.tr>
2020-12-14 08:53:38 +0000

    edited the variable name to make it more readable

--- Lab8/answers.py
+++ Lab8/answers.py
@@ -6,9 +6,11 @@ print("Hello world")
 
 #Question 1:
 a_list = [12, -7, 5, -89.4, 3, 27, 56, 57.3]
-def summation(lis):
+def summation(num_lis):
   sum = 0
-  for i in lis:
+  for i in num_lis:
     sum+=i
   return sum ** 2
 print(summation(a_list))
+
+#Question 2:
