a192ff2befe5783d1b55a0f1f0c3e65524b20a3f
r48982982 <malikal@ogr365.iyte.edu.tr>
2020-12-21 09:38:40 +0000

    My solution for question 1-4

--- Lab9/answers.py
+++ Lab9/answers.py
@@ -5,8 +5,9 @@ Section: 4
 Topic: Recursive & iterative functions
 This program is used as a medium for answers discussed in lab 9 
 """
+import time
 
-#Check that interpreter works:
+# Checking that interpreter works:
 print("Hello World")
 
 # Q1: harmonics
@@ -17,3 +18,29 @@ def harmonic(x):
     return harmonic(x-1) + 1/x
 
 print(harmonic(5))
+
+# Q2: reverse function
+def get_reversed(lis):
+  if len(lis) == 0:
+    return []
+  else:
+    return [lis[-1]]+ get_reversed(lis[:-1])
+
+print(get_reversed([1,2,3,4]))
+
+# Q4: number of evens
+def get_evens(lis):
+  if len(lis)==0:
+    return 0
+  else:
+    if lis[-1]%2 == 0:
+      return 1+get_evens(lis[:-1])
+    else:
+      return get_evens(lis[:-1])
+
+print(get_evens([0,1,2,3]))
+
+# Q5: Countdown
+def count_down(t):
+  if t == 0:
+    return 0
