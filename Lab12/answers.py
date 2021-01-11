7250c3d594a47d9682fb8b712e7451e9ea17cd3d
r48982982 <malikal@ogr365.iyte.edu.tr>
2021-01-11 09:02:19 +0000

    I edited the Cylinder class

--- Lab12/answers.py
+++ Lab12/answers.py
@@ -7,23 +7,36 @@ class Cylinder:
   def __init__(self,radius,height):
     self.radius = radius
     self.height = height
+
   def get_radius(self):
     return self.radius
+
   def get_height(self):
     return self.height
+
   def set_radius(self,radius):
     if radius > 0:
       self.radius = radius
+
   def set_height(self, height):
     if height > 0:
       self.height = height
+
   def baseArea(self):
     return math.pi * (self.radius ** 2)
+
+  def surfaceArea(self):
+    return 2 * math.pi * self.radius * self.height
+
   def area(self):
-    return 2 * self.baseArea() * self.height
+    return 2 * self.baseArea() + self.surfaceArea()
+
   def volume(self):
     return self.baseArea() * self.height
 
+anything = Cylinder(3, 5)
 
+print(anything.area())
+print(anything.volume())
 
 
