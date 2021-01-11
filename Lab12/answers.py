e136e44776508e7e27022c30c2ce6dc726b8c1b5
r48982982 <malikal@ogr365.iyte.edu.tr>
2021-01-11 09:24:24 +0000

    I added employee and company classes

--- Lab12/answers.py
+++ Lab12/answers.py
@@ -40,3 +40,53 @@ print(anything.area())
 print(anything.volume())
 
 
+class Employee:
+  def __init__(self,name,salary):
+    self.name = name
+    self.salary = salary
+  
+  def get_name(self):
+    return self.name
+  
+  def get_salary(self):
+    return self.salary
+  
+  def set_name(self, name):
+    self.name = name
+  
+  def set_salary(self, salary):
+    self.salary = salary
+  
+  def display_info(self):
+    print("Name: ", self.get_name())
+    print("Salary: ", self.get_salary())
+  
+
+class Company:
+  def __init__(self):
+    self.employee_list = []
+
+  def get_employee_list(self):
+    return self.employee_list
+  
+  def set_employee_list(self, employee_list):
+    self.employee_list = employee_list
+  
+  def addEmployee(self, new_employee):
+    if isinstance(new_employee, Employee):
+       self.employee_list.append(new_employee)
+  
+  def display_all_info(self):
+    for employee in self.employee_list:
+      employee.display_info()
+  
+  def average_salary(self):
+    total_salary = 0
+    for employee in self.employee_list:
+      total_salary += employee.salary
+    average = total_salary // len(self.employee_list)
+    return average
+
+
+
+
