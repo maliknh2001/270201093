54f42c951432a68f3726e82ed9bec2129417e92a
r48982982 <malikal@ogr365.iyte.edu.tr>
2021-01-11 08:55:09 +0000

    Created a class called Cylinder

--- Lab12/answers.py
+++ Lab12/answers.py
@@ -1,2 +1,29 @@
 # Checking code is working
 print("hello world")
+
+import math
+
+class Cylinder:
+  def __init__(self,radius,height):
+    self.radius = radius
+    self.height = height
+  def get_radius(self):
+    return self.radius
+  def get_height(self):
+    return self.height
+  def set_radius(self,radius):
+    if radius > 0:
+      self.radius = radius
+  def set_height(self, height):
+    if height > 0:
+      self.height = height
+  def baseArea(self):
+    return math.pi * (self.radius ** 2)
+  def area(self):
+    return 2 * self.baseArea() * self.height
+  def volume(self):
+    return self.baseArea() * self.height
+
+
+
+
