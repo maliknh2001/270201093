e4a747a555ebbd89c9f03ab6c30666688521488a
r48982982 <malikal@ogr365.iyte.edu.tr>
2021-01-18 09:19:52 +0000

    Finished binary search

--- Lab13/answers.py
+++ Lab13/answers.py
@@ -11,5 +11,19 @@ def selectionSort(lst):
     lst[0], lst[x] = x1, lst[0]
     return [lst[0]] + selectionSort(lst[1:])
 
-print(selectionSort([4,3,1,2,-1,-4,5,7,8]))
 
+def binarySearch(lst, item):
+  if len(lst) == 0:
+    return -1
+  else:
+    mid_point = len(lst)//2
+    smaller_list = lst[mid_point+1:] 
+    larger_list = lst[:mid_point-1]
+    if lst[mid_point] == item:
+      return mid_point
+    elif lst[mid_point] < item:
+      return binarySearch(larger_list,item) 
+    else:
+      return binarySearch(smaller_list, item) 
+
+print(binarySearch([1,2,3,4,5,6,7,8,9],5))
